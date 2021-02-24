3e52bf16b40022d4bc24d423d58d66597e2fd253
Tac-O-Lanthan <75772499+Tac-O-Lanthan@users.noreply.github.com>
2021-02-24 17:22:49 +0900

    roomコントローラー追記1

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -4,10 +4,27 @@ class RoomsController < ApplicationController
     @rooms = Room.all.includes(:user)
   end
 
+  # roomsコントローラーのshowアクションは、入室のためのパスワード入力画面表示として使う
+  def show
+  end
+
   def new
+    @room = Room.new
   end
 
   def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:room_name, :password_digest)
   end
 
 end
