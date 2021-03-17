2e4ebdd99a64763e45687d062e6652e0e3e69e16
Tac-O-Lanthan <75772499+Tac-O-Lanthan@users.noreply.github.com>
2021-03-17 15:52:49 +0900

    deviseのview作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,8 @@
 class RoomsController < ApplicationController
+  # index以外は全てログイン必須。ログイン機能完成までコメントアウト
+  # before_action :authenticate_user!, except: [:index]
   
+  # ホームページ
   def index
     @rooms = Room.all.includes(:user)
   end
@@ -8,10 +11,12 @@ class RoomsController < ApplicationController
   def show
   end
 
+  # ルーム作成画面
   def new
     @room = Room.new
   end
 
+  # 作成したルームの保存
   def create
     @room = Room.new(room_params)
     if @room.save
