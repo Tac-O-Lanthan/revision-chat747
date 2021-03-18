474a97a9d0ec43d105807cbd2135ba11ccb39215
Tac-O-Lanthan <75772499+Tac-O-Lanthan@users.noreply.github.com>
2021-03-18 20:53:00 +0900

    サインアップ画面更新エラー解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :email, uniqueness: true
+    validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?[0-9])[a-zA-Z0-9]{6,}\z/ }
+    validates :user_name, uniqueness: true
+    validates :full_name
+    validates :corp_name
+  end
 end
