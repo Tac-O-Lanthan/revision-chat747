02a00ed704b267b74e0a8a63f76935449974eff3
Tac-O-Lanthan <75772499+Tac-O-Lanthan@users.noreply.github.com>
2021-04-01 20:04:24 +0900

    微調整0401

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -20,7 +20,6 @@ class RoomsController < ApplicationController
   # 作成したルームの保存
   def create
     @room = Room.new(room_params)
-    binding.pry
     if @room.save
       redirect_to root_path
     else
